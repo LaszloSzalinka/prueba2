6f43c2f3538028b27832003bb7cb700c28ee2328
CAROLINA <CAROLINA@CAROLINA-PC>
2020-05-06 18:51:05 -0500

    laslo gay

--- prueba/src/prueba/main.java
+++ prueba/src/prueba/main.java
@@ -15,7 +15,8 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("camilo es gay");   
+        System.out.println("laslo es gay ");   
+        
     }
     
 }
